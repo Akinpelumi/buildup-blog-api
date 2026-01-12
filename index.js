ecf54a1b378de580e12a6110f9a8d895d1a01e5b
Akinpelumi <akintundeakinpelumi@gmail.com>
2026-01-12 14:41:29 +0100

    Add seed admin function

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const cn = {
   max: 1000
 };
 
-const db = pg(cn);
+export const db = pg(cn);
 
 app.get('/', (_req, res) => {
   res.status(200).json({ message: 'Welcome to BuildUp Backend Cohort One' });
@@ -417,12 +417,20 @@ app.post('/posts/:postId/comment', async(req, res) => {
     }
 })
 
-app.post('/posts/:postId/like', async(req, res) => {
+app.post('/posts/:postId', async(req, res) => {
     try {
-    const { params: { postId } } = req;
+    const { params: { postId }, query: { action } } = req;
 
     // decode the token
     const blogUser = await decodeToken(req, res);
+
+    if (!action || (action !== 'like' || action !== 'unlike')) {
+        return res.status(422).json({
+            status: 'error',
+            code: 422,
+            message: 'action query parameter is required and must be either like or unlike'
+        })
+    }
 
     const postExists = await db.oneOrNone(`SELECT id, title, content FROM blog_posts WHERE id = $1 AND status = 'published'`, [ postId ]);
         if (!postExists) {
@@ -433,14 +441,24 @@ app.post('/posts/:postId/like', async(req, res) => {
             })
         }
     
-    const postLike = await db.one('UPDATE blog_posts SET updated_at = NOW(), likes_count = likes_count + 1 WHERE id = $1 RETURNING id, title, content, status, likes_count, views_count', [ postId ])
+        if (action === 'like') {
+            const postLike = await db.one('UPDATE blog_posts SET updated_at = NOW(), likes_count = likes_count + 1 WHERE id = $1 RETURNING id, title, content, status, likes_count, views_count', [ postId ])
+            return res.status(200).json({
+                    status: 'success',
+                    code: 200,
+                    message: 'Post liked successfully',
+                    data: postLike
+            })
 
-    return res.status(200).json({
-            status: 'success',
-            code: 200,
-            message: 'Post liked successfully',
-            data: postLike
-        })
+        } else {
+            const postUnlike = await db.one('UPDATE blog_posts SET updated_at = NOW(), likes_count = likes_count - 1 WHERE id = $1 RETURNING id, title, content, status, likes_count, views_count', [ postId ])
+            return res.status(200).json({
+                    status: 'success',
+                    code: 200,
+                    message: 'Post unliked successfully',
+                    data: postUnlike
+            })
+        }
     } catch (error) {
         return res.status(500).json({
             status: 'error',
@@ -479,3 +497,34 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
 });
+
+// Assignment 1 
+
+// // add a new migration file 
+// 1.⁠ ⁠Remove the NOT NULL constraint on authors column on blog_posts table using ALTER TABLE command
+// 2.⁠ ⁠Insert into blog_posts table blog posts with with different statuses 
+
+// // Endpoints
+// 3.⁠ ⁠Create an endpoint to fetch all published blog posts
+// 4.⁠ ⁠Create an endpoint to comment on a blog post (Restricted to only verified and authenticated users)
+// 5.⁠ ⁠Create an endpoint to like AND unlike a blog post (Restricted to only verified and authenticated users) (Same endpoint using query params to handle the two scenarios for like and unlike)
+// 6.⁠ ⁠Create an endpoint to delete user own comment on a blog post (Restricted to only verified and authenticated users)
+
+
+// Assignment 2
+
+// // add a new migration file 
+//  7.⁠ ⁠Create a user_type enum with values "user" and "admin"
+//  8.⁠ ⁠Add a column user_type to blog_users table with Default value of “user”
+//  9.⁠ ⁠Seed an admin user into the blog_users table with user_type “admin” (Seed the hashed password for the admin, but remember the plain password)
+
+// // Endpoints
+// 10.⁠ ⁠Create an endpoint for admins to create a blog author profile (Strictly for logged in admins)
+// 11.⁠ ⁠Create an endpoint for admins to edit a blog author profile (Strictly for logged in admins)
+// 12.⁠ ⁠Create an endpoint for admins to delete a blog author profile (Strictly for logged in admins)
+// 13.⁠ ⁠Create an endpoint to fetch blog authors profile (Strictly for logged in admins)
+// 14.⁠ ⁠Create an endpoint to fetch single blog author profile (Strictly for logged in admins)
+// 15.⁠ ⁠Create an endpoint for admins to add blog posts (attached to an existing author on the platform) (Strictly for logged in admins)
+// 16.⁠ ⁠Create an endpoint for admins to edit a blog post (Strictly for logged in admins)
+// 17.⁠ ⁠Create an endpoint for admins to delete a blog post (Soft delete) (Strictly for logged in admins)
+// 18.⁠ ⁠Create an endpoint for all users to view extended details of a blog post (the blog authors details, the comments under the posts, the likes under the post)
